9448620c9257b763b8cd63e5f7408dc976749083
JuanesGH <jheras@2amigos.us>
2024-03-12 18:47:34 +0100

    start cotizador proyect

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import AppInsurance from "./components/AppInsurance";
+
+function App() {
+
+  return (
+    <AppInsurance />
+  )
+}
+
+export default App
---
+++ src/components/Form.jsx
@@ -0,0 +1,16 @@
+
+const Form = () => {
+  return (
+    <>
+        <form>
+            <div className="my-5">
+                <label className="block mb-3 font-bold text-gray-400 uppercase">
+                    Marca
+                </label>
+            </div>
+        </form>
+    </>
+  )
+}
+
+export default Form
